b57831cbd78da1c8ffb4827eaf0d213818378b2d
Juan Esteban Rivero Roche <juan.rivero@aireeterno.com>
2025-12-30 15:51:18 -0500

    fix: Update default MongoDB database name to DataAgendaStaging

--- Backend/app/database/mongo.py
+++ Backend/app/database/mongo.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 uri = os.getenv("MONGODB_URI")
-db_name = os.getenv("MONGODB_NAME", "DataAgenda")
+db_name = os.getenv("MONGODB_NAME", "DataAgendaStaging")
 
 if not uri:
     raise RuntimeError("MONGODB_URI no está definida en .env")
