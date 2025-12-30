f867a64f85018f098f5721854818f278cf3ec712
Juan Esteban Rivero Roche <juan.rivero@aireeterno.com>
2025-12-30 16:09:55 -0500

    fix: Correct CORS configuration for staging URL

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -30,8 +30,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://agenda.rizosfelices.co",
-        "https://staging-agenda.rizosfelices.co"
-        
+        "https://staging-agenda.rizosfelices.co",
     ],
     allow_credentials=True,
     allow_methods=["*"],
